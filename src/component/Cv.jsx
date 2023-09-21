1445e4a3a05626effd496de48da7871d5b9c8573
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-09-22 00:17:38 +0100

    commit

--- src/component/Cv.jsx
+++ src/component/Cv.jsx
@@ -11,7 +11,7 @@ const Cv = () => {
   return (
     <div data-aos="fade-up">
     <div className='mx-auto text-center mt-10 font-bold '>
-    <a className='bg-white p-2 rounded' href="https://drive.google.com/file/d/13luPpatG_DZcLTxwSSasEV-u-DLmgTnK/view?usp=sharing">DOWNLOAD CV</a>
+    <a className='bg-white p-2 rounded' href="https://docs.google.com/document/d/1dBhesOgUJ0YfcYq_yzmnThx5OoyTDs96/edit">DOWNLOAD CV</a>
     </div>
     
     </div>
