7a4ea8e61525e84b6cab93c29eacdc1e99ea4703
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-09-21 12:41:17 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AboutMe from "./component/AboutMe";
 import Skills from "./component/Skills";
 import NavBarDesktop from "./component/NavBarDesktop";
 import { FaBars } from 'react-icons/fa'
+// import NavBar from "./component/Navbar";
 import Middle from "./component/Middle";
 import Card from "./component/Card";
 import AOS from "aos";
@@ -13,8 +14,8 @@ import './index.css'
 import Cv from "./component/Cv";
 import Project from "./component/Project";
 import Contact from "./component/Contact";
-import NavBar from "./component/Navbar";
 import Footer from "./component/Footer";
+import Nav from "./component/Nav";
 
 function App() {
 
@@ -37,7 +38,7 @@ function App() {
         <div className="bg-lightBg sticky top-0 z-[100000]">
           <NavBarDesktop />
           <div className=" text-red-200 cursor-pointer text-[1rem] lg:hidden transition duration-[0.8s]">
-            <NavBar isOpen={isOpen} handleClose={toggleIsOpen} />
+            <Nav isOpen={isOpen} handleClose={toggleIsOpen} />
             <FaBars onClick={toggleIsOpen} />
           </div>
         </div>
