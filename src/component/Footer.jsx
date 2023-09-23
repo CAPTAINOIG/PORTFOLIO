7d016c0f2b688ae67252dbd34a888f12aef4e327
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-09-23 13:22:47 +0100

    commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <div className='text-center bg-white text-gray-900 font-bold p-1'>
+        <div className='text-center bg-white lg:my-5 text-gray-900 font-bold p-1'>
         Dev OIG @ 2023 - Your Favorite Front End Developer
         </div>
     </>
