ff5c935a4dcafb0c1f53056d8b1994bc985aa265
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-10-20 12:58:01 +0100

    commit

--- src/component/ProjectList.jsx
+++ src/component/ProjectList.jsx
@@ -34,7 +34,7 @@ export const ProjectList = [
     {
         image: "/image/product1.jpg",
         title: "React E-Commerce",
-        gitlink: "https://captainoig-mini-e-commerce.netlify.app",
+        gitlink: "https://captainoig-mini-e-commerce.netlify.app/",
         content: "Ecommerce is a method of buying and selling goods and services online. The definition of ecommerce business can also include tactics like affiliate marketing.",
     },
 ]
